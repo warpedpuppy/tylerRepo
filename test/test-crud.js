8bf97bdedee9c2f69e1b78bd0424406456b49b26
gotylergo <tk03759@gmail.com>
2018-03-15 21:15:50 -0700

    Create CRUD tests and add TravisCI

--- test/test-crud.js
+++ test/test-crud.js
@@ -23,4 +23,54 @@ describe('Blog', function() {
 			expect(res).to.have.status(200);
 		});
 	});
+
+	it('Should add a blog post on POST', function() {
+		let newPost = {
+			title:  'another blog',
+      		content: 'some more content',
+      		author: 'gotylergo'
+      	};
+		return chai.request(app)
+		.post('/blog-posts')
+		.send(newPost)
+		.then(function(res) {
+			expect(res).to.have.status(200);
+		});
+	});
+	it('Should update a blog post on POST', function() {
+		let newPost = {
+			title:  'changed blog',
+      		content: 'some different content',
+      		author: 'gotylergo'
+      	};
+		return chai.request(app)
+		.get('/blog-posts')
+		.then(function(res) {
+			newPost.id = res.body[0].id;
+			return chai.request(app)
+			.put('/blog-posts/'+ newPost.id)
+			.send(newPost)
+		})
+
+		.then(function(res) {
+			expect(res).to.have.status(200);
+		});
+	});
+
+	it('Should delete a blog post on DELETE', function() {
+		let newPost = {
+      	};
+		return chai.request(app)
+		.get('/blog-posts')
+		.then(function(res) {
+			newPost.id = res.body[0].id;
+			return chai.request(app)
+			.put('/blog-posts/'+ newPost.id)
+		})
+
+		.then(function(res) {
+			expect(res).to.have.status(200);
+		});
+	});
+
 });
